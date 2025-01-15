699466ef1ba9d5386665e61058bd5edaf134a5a9
Daniel72627 <danieldenava04@gmail.com>
2025-01-15 07:49:03 -0700

    s

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RecordTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RecordTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -14,7 +15,10 @@ import java.util.List;
 public class RecordTeleOp extends OpMode {
 
     public DcMotor BL, BR, FR, FL, SL, SR;
-    public boolean d_up, d_down, d_left, d_right;
+    public boolean d_up, d_down, d_left, d_right, a_pressed, last_a_state;
+    public Servo SWL,SWR,SC;
+    boolean a_pressed = false;  // Keep track of whether the button was toggled or not
+    boolean last_a_state = false;
     private List<InputLog> logData;
     private long startTime;
 
@@ -25,15 +29,25 @@ public class RecordTeleOp extends OpMode {
         BR = hardwareMap.get(DcMotor.class, "BR");
         FR = hardwareMap.get(DcMotor.class, "FR");
         FL = hardwareMap.get(DcMotor.class, "FL");
+
         SL = hardwareMap.get(DcMotor.class, "slide_left_motor");
         SR = hardwareMap.get(DcMotor.class, "slide_right_motor");
+
+        SWL = hardwareMap.get(Servo.class, "dsad");
+        SWR = hardwareMap.get(Servo.class, "Uhm idk");
+        SC = hardwareMap.get(Servo.class, "Uhm idk");
 
         BL.setDirection(DcMotor.Direction.FORWARD);
         BR.setDirection(DcMotor.Direction.FORWARD);
         FR.setDirection(DcMotor.Direction.FORWARD);
         FL.setDirection(DcMotor.Direction.FORWARD);
+
         SL.setDirection(DcMotor.Direction.FORWARD);
         SR.setDirection(DcMotor.Direction.FORWARD);
+
+        SWL.setDirection(Servo.Direction.FORWARD);
+        SWR.setDirection(Servo.Direction.REVERSE);
+        SC.setDirection(Servo.Direction.FORWARD);
 
 
         logData = new ArrayList<>();
@@ -53,6 +67,7 @@ public class RecordTeleOp extends OpMode {
         d_down = gamepad1.dpad_down;
         d_right = gamepad1.dpad_right;
         d_left = gamepad1.dpad_left;
+        boolean current_a_state = gamepad1.a;
 
 
         if (d_up) {
@@ -78,6 +93,30 @@ public class RecordTeleOp extends OpMode {
         SL.setPower(gamepad1.right_stick_y);
         SR.setPower(gamepad1.right_stick_y);
 
+        if(gamepad1.right_trigger > .3) {
+            SWL.setPosition(.2);
+            SWR.setPosition(.2);
+        }
+
+        if(gamepad1.left_trigger > .3) {
+            SWL.setPosition(-.2);
+            SWR.setPosition(-.2);
+        }
+
+        if (current_a_state && !last_a_state) {  // If the A button was just pressed
+            a_pressed = !a_pressed;  // Toggle the arm state
+
+            // Toggle the arm based on the value of a_pressed
+            if (a_pressed) {
+                SC.setPosition(0.2);  // Open the arm
+            } else {
+                SC.setPosition(-0.2);  // Close the arm
+            }
+        }
+
+        // Update last_a_state to the current state of the A button for next loop
+        last_a_state = current_a_state;
+
         //---------------------------------------------------------LEAVE THIS ALONE
         double BLPower = BL.getPower();
         double BRPower = BR.getPower();
@@ -86,6 +125,10 @@ public class RecordTeleOp extends OpMode {
 
         double SLPower = SL.getPower();
         double SRPower = SR.getPower();
+
+        double SWLPos = SWL.getPosition();
+        double SWRPos = SWR.getPosition();
+        double SCPos = SC.getPosition();
 
         InputLog log = new InputLog();
         log.timestamp = System.currentTimeMillis() - startTime;
@@ -98,6 +141,10 @@ public class RecordTeleOp extends OpMode {
         log.SLPower = SLPower;
         log.SRPower = SRPower;
 
+        log.SWLPos = SWLPos;
+        log.SWRPos = SWRPos;
+        log.SCPos = SCPos;
+
         logData.add(log);
 
         telemetry.addData("Recording", "Timestamp: %d ms", log.timestamp);
@@ -107,6 +154,9 @@ public class RecordTeleOp extends OpMode {
         telemetry.addData("FL Power", FLPower);
         telemetry.addData("SL Power", SLPower);
         telemetry.addData("SR Power", SRPower);
+        telemetry.addData("SWL Power", SWLPos);
+        telemetry.addData("SWR Power", SWRPos);
+        telemetry.addData("SC", SCPos);
         telemetry.update();
     }
 
@@ -124,8 +174,8 @@ public class RecordTeleOp extends OpMode {
 
         try (FileWriter writer = new FileWriter(filePath)) {
             for (InputLog log : logData) {
-                writer.write(String.format("Timestamp: %d, BLPower: %.2f, BRPower: %.2f, FRPower: %.2f, FLPower: %.2f, SLPower: %.2f, SRPower: %.2f\n",
-                        log.timestamp, log.BLPower, log.BRPower, log.FRPower, log.FLPower, log.SLPower, log.SRPower));
+                writer.write(String.format("Timestamp: %d, BLPower: %.2f, BRPower: %.2f, FRPower: %.2f, FLPower: %.2f, SLPower: %.2f, SRPower: %.2f\n, SWLPos: %.2f\n, SWRPos: %.2f\n, SCPos: %.2f\n",
+                        log.timestamp, log.BLPower, log.BRPower, log.FRPower, log.FLPower, log.SLPower, log.SRPower, log.SWLPos, log.SWRPos, log.SCPos));
             }
             telemetry.addData("Status: ", "Recording saved to %s", filePath);
         } catch (IOException e) {
@@ -143,6 +193,9 @@ public class RecordTeleOp extends OpMode {
         double FLPower;
         double SLPower;
         double SRPower;
+        double SWLPos;
+        double SWRPos;
+        double SCPos;
     }
 
     public void setMotors(int fl, int fr, int br, int bl) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Motors.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Test_Motors extends LinearOpMode {
 
     public DcMotor BL, BR, FR, FL, SL, SR;
-    public Servo servo_wrist, servo_claw;
+    public Servo servo_wrist_l,servo_wrist_r, servo_claw;
 
 
     @Override
@@ -24,7 +24,8 @@ public class Test_Motors extends LinearOpMode {
         SR = hardwareMap.get(DcMotor.class, "slide_right");
         SL = hardwareMap.get(DcMotor.class, "slide_left");
         servo_claw = hardwareMap.get(Servo.class, "Uhm idk");
-        servo_wrist = hardwareMap.get(Servo.class, "Uhm idk");
+        servo_wrist_l = hardwareMap.get(Servo.class, "dsad");
+        servo_wrist_r = hardwareMap.get(Servo.class, "Uhm idk");
 
         BL.setDirection(DcMotor.Direction.FORWARD);
         BR.setDirection(DcMotor.Direction.FORWARD);
@@ -32,7 +33,8 @@ public class Test_Motors extends LinearOpMode {
         FL.setDirection(DcMotor.Direction.FORWARD);
         SL.setDirection(DcMotor.Direction.FORWARD);
         SR.setDirection(DcMotor.Direction.FORWARD);
-        servo_wrist.setDirection(Servo.Direction.FORWARD);
+        servo_wrist_l.setDirection(Servo.Direction.FORWARD);
+        servo_wrist_r.setDirection(Servo.Direction.REVERSE);
         servo_claw.setDirection(Servo.Direction.FORWARD);
 
 
@@ -49,17 +51,18 @@ public class Test_Motors extends LinearOpMode {
         sleep(2000);
         BL.setPower(0);
         SL.setPower(.1);
-        sleep(1000);
-        SL.setPower(0);
         SR.setPower(.1);
         sleep(1000);
+        SL.setPower(0);
         SR.setPower(0);
-        servo_wrist.setPosition(100);
+        servo_wrist_l.setPosition(.2);
+        servo_wrist_r.setPosition(.2);
         sleep(4000);
-        servo_claw.setPosition(60);
+        servo_wrist_l.setPosition(0);
+        servo_wrist_r.setPosition(0);
+        servo_claw.setPosition(.2); // POSITIVE CLOSE
         sleep(3000);
-
-
+        servo_claw.setPosition(0);
 
 
     }
